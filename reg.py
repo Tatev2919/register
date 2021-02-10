21160556f5b1b95d3506447b4529af57a8121e8e
Tatev2919 <66516027+Tatev2919@users.noreply.github.com>
2021-02-10 17:52:10 +0400

    Update reg.py

--- reg.py
+++ reg.py
@@ -1,6 +1,7 @@
 import re
 
-users = [{"userName": 'John', 'passwd': 'johnjames97'}, {'userName': 'Jane', 'passwd': 'jane9815'}];
+users = {1: {"userName": 'John', 'passwd': 'johnjames97'},
+         2: {'userName': 'Jane', 'passwd': 'jane9815'}};
 
 name = input("Type your name")
 while (re.match("^[ a-zA-Z0-9_.-]+$", name) == None):
@@ -10,13 +11,19 @@ passwd = input("Type your passwd")
 while (re.match(r'[A-Za-z0-9@#$%^&+=]{5,}', passwd) == None):
     passwd = input("Type your passwd correctly")
 changeVal = 0
-#flag = False
+
 regOrLog = input("Registration or Login ? // if registration then type 0 ,else 1 ")
+print("enter 0 or 1")
+while(regOrLog not in ('0','1')):
+    regOrLog = input("Registration or Login ? // if registration then type 0 ,else 1 ")
+
 if regOrLog == '1':
     flag = False
-    for i in users:
-        if name in i.get('userName'):
-            if passwd in i.get('passwd'):
+    for key in users.keys():
+        print(users[key])
+        print(users[key].get("userName"))
+        if name == users[key].get('userName'):
+            if passwd == users[key].get('passwd'):
                 print("Congrats you've logged successfully!!")
                 flag = True
                 break
@@ -27,10 +34,11 @@ if regOrLog == '1':
             print("your input is not correct")
             changeVal = input("enter yes or no ? ")
         if (changeVal == "yes"):
-            regOrLog = 0
+            regOrLog = '0'
         if (changeVal == "no"):
             print("Byee!")
 if regOrLog == '0':
     user1 = dict({'userName': name, 'passwd': passwd});
-    users.append(dict(user1))
-    print(users)
+    users['4'] = (dict(user1))
+    for key1 in users.keys():
+        print(users[key1])
